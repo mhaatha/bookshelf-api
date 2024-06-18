05f68dc43bf142c83e34ec126f42e2c0a39bd422
Hafidz Athaya <mhafidza528@gmail.com>
2024-06-18 13:40:18 +0800

    implement Joi validation to routing

--- src/routes/book.route.js
+++ src/routes/book.route.js
@@ -1,29 +1,37 @@
 const { addBookToBookshelf, getAllBooks, getBookById, updateBook, deleteBook } = require('../handlers/book.handler');
+const { bookshelfSchema } = require('../validations/book.validate');
+const { postBookErrorHandler } = require('../errors/book.error');
 
 module.exports = [
   {
     method: 'POST',
     path: '/books',
     handler: addBookToBookshelf,
+    options: {
+      validate: {
+        payload: bookshelfSchema,
+        failAction: postBookErrorHandler,
+      },
+    },
   },
   {
     method: 'GET',
     path: '/books',
-    handler: getAllBooks
+    handler: getAllBooks,
   },
   {
     method: 'GET',
     path: '/books/{bookId}',
-    handler: getBookById
+    handler: getBookById,
   },
   {
     method: 'PUT',
     path: '/books/{bookId}',
-    handler: updateBook
+    handler: updateBook,
   },
   {
     method: 'DELETE',
     path: '/books/{bookId}',
-    handler: deleteBook
-  }
+    handler: deleteBook,
+  },
 ];
